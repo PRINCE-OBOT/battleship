58317084502f3e4fd9272c6b0105917cafbec67c
PRINCE-OBOT <princessamuel45@gmail.com>
2026-02-17 00:14:23 +0100

    test: add test for  hit and isSunk

---
+++ tests/ship.test.js
@@ -0,0 +1,35 @@
+import Ship from '../src/ship';
+
+describe('Ship', () => {
+  test('hit once', () => {
+    const ship = Ship();
+    ship.hit();
+    expect(ship.getHit()).toBe(1);
+  });
+  test('hit many times', () => {
+    const ship = Ship();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.getHit()).toBe(3);
+  });
+  test('hit is less than length', () => {
+    const ship = Ship(2);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+  test('hit is equal to length', () => {
+    const ship = Ship(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+  test('hit is greater than length', () => {
+    const ship = Ship(2);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
+Ship;
