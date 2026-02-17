10632d203246b6a59c9ea40eb2aa294af7d42439
PRINCE-OBOT <princessamuel45@gmail.com>
2026-02-17 21:18:37 +0100

    test: update test
    - Remove redundant test
    - Add test to ensure that ship axis does not swap
    when it encounter a ship during swapping

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -2,25 +2,7 @@ import GameBoard from '../src/gameBoard';
 import Ship from '../src/ship';
 
 describe('Game Board', () => {
-  test('Ship with single length', () => {
-    const gameBoard = GameBoard();
-    gameBoard.setShip(Ship(1), 'j0');
-
-    expect(gameBoard.getShip('j0')).toBeTruthy();
-    expect(gameBoard.getShip('j1')).toBeFalsy();
-    expect(gameBoard.getShip('h0')).toBeFalsy();
-  });
-
-  test('Ship with double length with Default axis X', () => {
-    const gameBoard = GameBoard();
-    gameBoard.setShip(Ship(2), 'j0');
-
-    expect(gameBoard.getShip('j0')).toBeTruthy();
-    expect(gameBoard.getShip('j1')).toBeTruthy();
-    expect(gameBoard.getShip('i0')).toBeFalsy();
-  });
-
-  test('Ship with double length, switched to Y Axis', () => {
+  test('Ship with multiple length, switched from X to Y Axis', () => {
     const gameBoard = GameBoard();
     gameBoard.setShip(Ship(2), 'j0');
 
@@ -31,15 +13,15 @@ describe('Game Board', () => {
     expect(gameBoard.getShip('i0')).toBeTruthy();
   });
 
-  test('Ship with double length, switched to X Axis', () => {
+  test.only('If a cell is encounter when switching, do not switch', () => {
     const gameBoard = GameBoard();
-    gameBoard.setShip(Ship(2), 'j0');
+    gameBoard.setShip(Ship(3, 'c0'), 'c0');
+    gameBoard.setShip(Ship(1, 'a0'), 'a0');
 
-    gameBoard.switchShipAxis('j0');
-    gameBoard.switchShipAxis('j0');
+    gameBoard.switchShipAxis('c0');
 
-    expect(gameBoard.getShip('j0')).toBeTruthy();
-    expect(gameBoard.getShip('j1')).toBeTruthy();
-    expect(gameBoard.getShip('i0')).toBeFalsy();
+    expect(gameBoard.getShip('c0')).toBeTruthy();
+    expect(gameBoard.getShip('c1')).toBeTruthy();
+    expect(gameBoard.getShip('c2')).toBeTruthy();
   });
 });
