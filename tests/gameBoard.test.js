277837e5bb489681f5761f0196074a83cae4fef9
PRINCE-OBOT <princessamuel45@gmail.com>
2026-02-17 22:04:34 +0100

    test: ensure ship hit increment only when it is hitting

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -13,7 +13,7 @@ describe('Game Board', () => {
     expect(gameBoard.getShip('i0')).toBeTruthy();
   });
 
-  test.only('If a cell is encounter when switching, do not switch', () => {
+  test('If a cell is encounter when switching, do not switch', () => {
     const gameBoard = GameBoard();
     gameBoard.setShip(Ship(3, 'c0'), 'c0');
     gameBoard.setShip(Ship(1, 'a0'), 'a0');
@@ -23,5 +23,16 @@ describe('Game Board', () => {
     expect(gameBoard.getShip('c0')).toBeTruthy();
     expect(gameBoard.getShip('c1')).toBeTruthy();
     expect(gameBoard.getShip('c2')).toBeTruthy();
+  });
+
+  test.only('Increment hit when ship is hitting', () => {
+    const gameBoard = GameBoard();
+    const ship = Ship(3, 'c0')
+    gameBoard.setShip(ship, 'c0');
+
+    gameBoard.receiveAttack('c0');
+    gameBoard.receiveAttack('c4');
+
+    expect(ship.getHit()).toBe(1);
   });
 });
